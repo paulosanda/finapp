5bba31938ee5279493bd9bfbff8c263b72ef844e
paulosanda <paulosanda@gmail.com>
2023-10-03 16:48:27 -0300

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Example for response examples value"
+ * )
+ * @OA\PathItem(path="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
